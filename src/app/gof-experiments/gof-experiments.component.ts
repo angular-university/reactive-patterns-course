9d5f8027e268479b8758178d50f1c919834ff65e
JHades <jhades.dev@gmail.com>
2017-02-14 13:57:27 +0100

    reactive patterns course

--- src/app/gof-experiments/gof-experiments.component.ts
+++ src/app/gof-experiments/gof-experiments.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'gof-experiments',
-  templateUrl: './gof-experiments.component.html',
-  styleUrls: ['./gof-experiments.component.css']
+    selector: 'gof-experiments',
+    templateUrl: './gof-experiments.component.html',
+    styleUrls: ['./gof-experiments.component.css']
 })
 export class GofExperimentsComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+    hoverSection: HTMLElement;
+
+
+    ngOnInit() {
+
+        this.hoverSection = document.getElementById('gof-hover');
+
+        this.hoverSection.addEventListener('mousemove', onMouseMove);
+
+        this.hoverSection.addEventListener('click', onClick);
+
+    }
+
+    removeListeners() {
+
+        console.log("Removing listener");
+        this.hoverSection.removeEventListener('mousemove', onMouseMove);
+    }
 
 }
+
+
+function onMouseMove(ev: MouseEvent) {
+    console.log("mouse move", ev);
+}
+
+
+function onClick(ev: MouseEvent) {
+    console.log("click", ev);
+}
+
