b4c8a17ee91e37712f219664cbf416dc865da038
Your Name <you@example.com>
2017-02-15 15:13:07 +0100

    introduction to reactive programming

--- src/app/browser-event-experiments/browser-event-experiments.component.ts
+++ src/app/browser-event-experiments/browser-event-experiments.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrowserEventExperimentsComponent implements OnInit {
 
-  constructor() { }
+    hoverSection: HTMLElement;
+
 
   ngOnInit() {
+
+      this.hoverSection = document.getElementById('hover');
+
+      this.hoverSection.addEventListener('mousemove', ev => {
+         console.log(ev);
+      });
+
   }
 
 }
