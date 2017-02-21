f872bafa9a8673c443639b6ca09fee32522af16b
Your Name <you@example.com>
2017-02-21 14:53:59 +0100

    add multiple events, demo how event bus dont scale in complexity

--- src/app/event-bus-experiments/event-bus-experiments.component.ts
+++ src/app/event-bus-experiments/event-bus-experiments.component.ts
@@ -1,24 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {globalEventBus, LESSONS_LIST_AVAILABLE, ADD_NEW_LESSON} from "./event-bus";
 import {testLessons} from "../shared/model/test-lessons";
 
 @Component({
-  selector: 'event-bus-experiments',
-  templateUrl: './event-bus-experiments.component.html',
-  styleUrls: ['./event-bus-experiments.component.css']
+    selector: 'event-bus-experiments',
+    templateUrl: './event-bus-experiments.component.html',
+    styleUrls: ['./event-bus-experiments.component.css']
 })
 export class EventBusExperimentsComponent implements OnInit {
 
 
+    ngOnInit() {
 
-  ngOnInit() {
+        console.log('Top level component broadcasted all lessons ...');
 
-      console.log('Top level component broadcasted all lessons ...');
+        globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE,
+            testLessons.slice(0));
 
-      globalEventBus.notifyObservers(LESSONS_LIST_AVAILABLE,
-          testLessons.slice(0));
-
-  }
+    }
 
     addLesson(lessonText: string) {
         globalEventBus.notifyObservers(ADD_NEW_LESSON, lessonText);
