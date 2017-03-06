0a12e531b1e0d04a666386c2ed3b2ee3a6d10f5e
Your Name <you@example.com>
2017-03-06 17:20:41 +0100

    observable stateless service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,35 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AngularFireDatabase} from 'angularfire2';
 import {Course} from "../shared/model/course";
+import {Lesson} from "../shared/model/lesson";
 
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  courses: Course[];
+    courses: Course[];
+    latestLessons: Lesson[];
 
+    constructor(private db: AngularFireDatabase) {
 
-  constructor(private db: AngularFireDatabase) {
+    }
 
-  }
+    ngOnInit() {
 
+        this.db.list('courses')
+            .do(console.log)
+            .subscribe(
+                data => this.courses = data
+            );
 
-  ngOnInit() {
-
-     this.db.list('courses')
-        .do(console.log)
-        .subscribe(
-            data => this.courses = data
-        );
-  }
-
-
-    changeCourseData() {
-      this.courses.forEach(course => course.description  = '=>' + course.description  );
+        this.db.list('lessons', {
+            query: {
+                orderByKey: true,
+                limitToLast: 10
+            }
+        })
+            .do(console.log)
+            .subscribe(
+                data => this.latestLessons = data
+            );
     }
 
 }
