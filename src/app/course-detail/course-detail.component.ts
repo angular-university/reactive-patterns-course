21c771b914b1b67c4a1c6f0de0f19669e0acb2e4
Your Name <you@example.com>
2017-03-16 12:53:11 +0100

    preparing new lessons

--- src/app/course-detail/course-detail.component.ts
+++ src/app/course-detail/course-detail.component.ts
@@ -5,6 +5,7 @@ import {Lesson} from "../shared/model/lesson";
 import * as _ from 'lodash';
 import {CoursesService} from "../services/courses.service";
 import {NewsletterService} from "../services/newsletter.service";
+import {UserService} from "../services/user.service";
 
 
 @Component({
@@ -19,22 +20,8 @@ export class CourseDetailComponent implements OnInit {
 
   constructor(private route: ActivatedRoute,
               private coursesService: CoursesService,
-              private newsletterService: NewsletterService) {
-
-      route.params
-          .subscribe( params => {
-
-              const courseUrl = params['id'];
-
-              this.coursesService.findCourseByUrl(courseUrl)
-              .subscribe(data => {
-                  this.course = data;
-
-                  this.coursesService.findLessonsForCourse(this.course.id)
-                    .subscribe(lessons => this.lessons = lessons);
-              });
-
-          });
+              private newsletterService: NewsletterService,
+              private userService:UserService) {
 
   }
 
@@ -49,7 +36,20 @@ export class CourseDetailComponent implements OnInit {
   }
 
   ngOnInit() {
+      this.route.params
+          .subscribe( params => {
 
+              const courseUrl = params['id'];
+
+              this.coursesService.findCourseByUrl(courseUrl)
+                  .subscribe(data => {
+                      this.course = data;
+
+                      this.coursesService.findLessonsForCourse(this.course.id)
+                          .subscribe(lessons => this.lessons = lessons);
+                  });
+
+          });
   }
 
 }
