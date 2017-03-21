17f6edd4d493ae5fd216d9da749d5bee6c410aa3
Your Name <you@example.com>
2017-03-21 11:31:30 +0100

    master detail implementation

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,61 +1,44 @@
 import {Component, OnInit, Input, OnDestroy} from '@angular/core';
-import {LessonsPager} from "../services/lessons-pager.service";
 import {Observable} from "rxjs";
 import {Lesson} from "../shared/model/lesson";
 import {CoursesHttpService} from "../services/courses-http.service";
-import {Course} from "../shared/model/course";
 
 @Component({
     selector: 'course',
     templateUrl: './course.component.html',
-    styleUrls: ['./course.component.css'],
-    providers: [LessonsPager]
+    styleUrls: ['./course.component.css']
 })
 export class CourseComponent implements OnInit, OnDestroy {
 
     @Input()
     id: number;
 
-    course$: Observable<Course>;
-
-    lessons$: Observable<Lesson[]>;
-
-    detail$: Observable<Lesson>;
-
-    constructor(
-        private lessonsPager: LessonsPager,
-        private coursesService: CoursesHttpService) {
+    constructor(private coursesService: CoursesHttpService) {
 
     }
 
     ngOnInit() {
 
-        this.course$ = this.coursesService.findCourseById(this.id);
-
-        this.lessons$ = this.lessonsPager.lessonsPage$;
-
-        this.lessonsPager.loadFirstPage(this.id);
     }
 
     previousLessonsPage() {
-        this.lessonsPager.loadPreviousPage();
+
     }
 
     nextLessonsPage() {
-        this.lessonsPager.loadNextPage();
+
     }
 
     backToMaster() {
-        this.detail$ = undefined;
+
     }
 
     selectDetail(lesson:Lesson) {
-        this.detail$ = this.coursesService.findLessonDetailById(lesson.url);
+
     }
 
     ngOnDestroy() {
         console.log('destroying CourseComponent ...');
     }
-
 
 }
