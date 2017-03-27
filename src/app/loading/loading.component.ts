9f76cb1d151d3967738421c9a82a5dad2fae3496
Your Name <you@example.com>
2017-03-27 14:37:46 +0200

    RxJs and Reactive Patterns Angular Architecture Course ongoing

--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {Router, NavigationStart, RoutesRecognized} from "@angular/router";
 
 @Component({
-  selector: 'loading',
-  templateUrl: './loading.component.html',
-  styleUrls: ['./loading.component.css']
+    selector: 'loading',
+    templateUrl: './loading.component.html',
+    styleUrls: ['./loading.component.css']
 })
 export class LoadingComponent implements OnInit {
 
-  loading$: Observable<boolean>;
+    loading$: Observable<boolean>;
 
-  constructor(private router: Router) {
+    constructor(private router: Router) {
 
-  }
+    }
 
-  ngOnInit() {
-      this.loading$ = this.router.events
-          .map(event =>  event instanceof NavigationStart || event instanceof RoutesRecognized);
-  }
+    ngOnInit() {
+        this.loading$ = this.router.events
+            .map(event => event instanceof NavigationStart || event instanceof RoutesRecognized);
+    }
 
 }
