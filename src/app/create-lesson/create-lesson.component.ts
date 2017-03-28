a35238ec80149502be00c426a7cb4ffbc62fd2fd
Your Name <you@example.com>
2017-03-28 14:38:54 +0200

    RxJs and Reactive Patterns Angular Architecture Course ongoing

--- src/app/create-lesson/create-lesson.component.ts
+++ src/app/create-lesson/create-lesson.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
+import * as Cookies from 'cookies-js';
 
 @Component({
     selector: 'create-lesson',
@@ -7,6 +8,8 @@ import {FormGroup, FormBuilder, Validators} from "@angular/forms";
     styleUrls: ['./create-lesson.component.css']
 })
 export class CreateLessonComponent implements OnInit {
+
+    private static readonly DRAFT_COOKIE = 'create-lesson-draft';
 
     form: FormGroup;
 
@@ -22,6 +25,26 @@ export class CreateLessonComponent implements OnInit {
 
     ngOnInit() {
 
+        const draft = Cookies.get(CreateLessonComponent.DRAFT_COOKIE);
+
+        if (draft) {
+            this.form.setValue(JSON.parse(draft));
+        }
+
+        this.form.valueChanges
+            .filter(() => this.form.valid)
+            .do(validValue => Cookies.set(
+                CreateLessonComponent.DRAFT_COOKIE,
+                JSON.stringify(validValue)) )
+            .subscribe();
+
     }
 
 }
+
+
+
+
+
+
+
