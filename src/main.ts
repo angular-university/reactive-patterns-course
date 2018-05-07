119aedeabba1f3afab47574d1e8ee4fc1032e2dc
JHades <jhades.dev@gmail.com>
2018-05-07 15:03:49 +0200

    Course updated to Angular 6
    # Conflicts:
    #	README.md
    #	src/app/browser-event-experiments/browser-event-experiments.component.ts
    #	src/app/course-detail/course-detail.resolver.ts
    #	src/app/services/courses.service.ts
    #	src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,18 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/first';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/publishLast';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/withLatestFrom';
+
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
