c8b6b4bf2f5c9b0cbf38cc763ee6a87ca3b9421c
JHades <jhades.dev@gmail.com>
2018-10-24 13:13:05 +0200

    Upgrade to Angular 7
    # Conflicts:
    #	src/app/app.module.ts
    #	src/app/services/courses.service.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { LessonsListComponent } from './lessons-list/lessons-list.component';
 import { LessonsCounterComponent } from './lessons-counter/lessons-counter.component';
 import { HomeComponent } from './home/home.component';
 import {firebaseConfig} from "../environments/firebase.config";
-import {AngularFireModule} from 'angularfire2';
+import {AngularFireModule} from '@angular/fire';
 import {RouterModule} from '@angular/router';
 import {routerConfig} from "./router.config";
 import { CourseDetailComponent } from './course-detail/course-detail.component';
@@ -21,8 +21,8 @@ import {NewsletterService} from "./services/newsletter.service";
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { LoginComponent } from './login/login.component';
 import {UserService} from "./services/user.service";
-import {AngularFireAuthModule} from "angularfire2/auth";
-import {AngularFireDatabaseModule} from "angularfire2/database";
+import {AngularFireAuthModule} from "@angular/fire/auth";
+import {AngularFireDatabaseModule} from "@angular/fire/database";
 
 @NgModule({
   declarations: [
